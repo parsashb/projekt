6d9cfa19be19531ca9c97e1213769e05a8755d6e
Dumanenami <111508674+Dumanenami@users.noreply.github.com>
2024-07-18 19:57:51 +0330

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,10 @@
+let temp = 2;
+if (temp < 20) {
+  console.log("cold");
+} else if (30 < temp > 40) {
+  console.log("warm");
+} else if (35 < temp > 37) {
+  console.log("mild");
+} else if (temp > 40) {
+  console.log("hot");
+}
